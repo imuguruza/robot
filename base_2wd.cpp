cb3eb6bad6eb569157cb905ecdf25b60ef2f2921
imuguruza <inigo.muguruza89@gmail.com>
2017-03-18 17:29:40 +0100

    Add new ctrl fn

--- base_2wd.cpp
+++ base_2wd.cpp
@@ -11,19 +11,47 @@ void Base::stop(){
   MotorA.stop();
   MotorB.stop();
 }
-void Base::forward(){
+void Base::forward_steady(){
   MotorA.forward();
   MotorB.forward();
 }
+void Base::forward(){
+  MotorA.forward();
+  MotorB.forward();
+  std::this_thread::sleep_for(std::chrono::microseconds(100000));
+  MotorA.stop();
+  MotorB.stop();
+}
 void Base::backward(){
+  MotorA.backward();
+  MotorB.backward();
+  std::this_thread::sleep_for(std::chrono::microseconds(100000));
+  MotorA.stop();
+  MotorB.stop();
+}
+void Base::backward_steady(){
   MotorA.backward();
   MotorB.backward();
 }
 void Base::right(){
   MotorA.forward();
   MotorB.backward();
+  std::this_thread::sleep_for(std::chrono::microseconds(100000));
+  MotorA.stop();
+  MotorB.stop();
+}
+void Base::right_steady(){
+  MotorA.forward();
+  MotorB.backward();
 }
 void Base::left(){
+  MotorA.backward();
+  MotorB.forward();
+  std::this_thread::sleep_for(std::chrono::microseconds(100000));
+  MotorA.stop();
+  MotorB.stop();
+}
+void Base::left_steady(){
   MotorA.backward();
   MotorB.forward();
 }
