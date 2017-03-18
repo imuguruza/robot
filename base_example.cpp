711acf12da38583b4ac712859fb8e55e286eb66c
imuguruza <inigo.muguruza89@gmail.com>
2017-03-18 17:29:02 +0100

    Remove sleeps

--- base_example.cpp
+++ base_example.cpp
@@ -8,16 +8,12 @@ int main(int argc, char const *argv[]) {
   Base base;
   std::cout << "Go base forward..." << '\n';
   base.forward();
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   std::cout << "Go base backward..." << '\n';
   base.backward();
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   std::cout << "Go base left..." << '\n';
   base.left();
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   std::cout << "Go base right..." << '\n';
   base.right();
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   std::cout << "Go base stop..." << '\n';
   base.stop();
 
